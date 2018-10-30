6877273cc9d12be3c3bbab1669552d5a0846ce8b
Aubrey Lavigne <Aubrey.S.Lavigne@gmail.com>
2018-10-29 23:51:49 -0400

    Isolate Certificate lookup to its own file

--- main.go
+++ main.go
@@ -4,23 +4,13 @@ import (
 	"fmt"
 	"os"
 	"sync"
-	"time"
 
-	certLookup "github.com/genkiroid/cert"
+	"certcheck/cert"
+
 	"github.com/olekukonko/tablewriter"
 )
 
-// result is the returned value of the certificate lookup
-type result struct {
-	domain string
-	cert   certLookup.Cert
-
-	status  string
-	err     error
-	details string
-}
-
-const numWorkers = 4
+const numWorkers = 3
 
 func main() {
 	domains := []string{
@@ -30,7 +20,7 @@ func main() {
 		"reddit.com",
 	}
 
-	resultChan := make(chan result)
+	resultChan := make(chan cert.Certificate)
 	inputChan := make(chan string)
 
 	wg := &sync.WaitGroup{}
@@ -62,17 +52,17 @@ func main() {
 	errorTable.SetHeader([]string{"Name", "Status", "Error"})
 
 	for res := range resultChan {
-		if res.err != nil {
+		if res.Error != nil {
 			errorTable.Append([]string{
-				res.domain,
-				res.status,
-				fmt.Sprintf("%s", res.err),
+				res.Domain,
+				res.Status,
+				fmt.Sprintf("%s", res.Error),
 			})
-		} else if res.status != "" {
+		} else if res.Status != "" {
 			statusTable.Append([]string{
-				res.domain,
-				res.status,
-				res.details,
+				res.Domain,
+				res.Status,
+				res.Details,
 			})
 		}
 	}
@@ -82,63 +72,9 @@ func main() {
 	errorTable.Render()
 }
 
-func certStatusWorker(inputChan <-chan string, resultChan chan<- result, wg *sync.WaitGroup) {
+func certStatusWorker(inputChan <-chan string, resultChan chan<- cert.Certificate, wg *sync.WaitGroup) {
 	for domain := range inputChan {
-		resultChan <- getCertStatus(domain)
+		resultChan <- cert.LoadCertificate(domain)
 	}
 	wg.Done()
-}
-
-func getCertStatus(domain string) result {
-
-	cert := certLookup.NewCert(domain)
-	res := result{
-		domain: domain,
-		cert:   *cert,
-	}
-
-	if cert.Error != "" {
-		res.status = "SSL Lookup Error"
-		res.err = fmt.Errorf(cert.Error)
-		return res
-	}
-
-	certRange, err := getCertValidRange(res.cert)
-	if err != nil {
-		res.err = err
-	} else if certRange.contains(time.Now()) {
-		res.status = "Valid"
-	} else {
-		res.status = "Expired"
-	}
-
-	res.details = certRange.End.String()
-	return res
-}
-
-type dateRange struct {
-	Start time.Time
-	End   time.Time
-}
-
-func (d *dateRange) contains(t time.Time) bool {
-	return d.Start.Before(t) && t.Before(d.End)
-}
-
-func getCertValidRange(cert certLookup.Cert) (dateRange, error) {
-
-	startTime, err := time.Parse("2006-01-02 15:04:05 Z0700 MST", cert.NotBefore)
-	if err != nil {
-		return dateRange{}, fmt.Errorf("Start time parse error - %s", err)
-	}
-
-	endTime, err := time.Parse("2006-01-02 15:04:05 Z0700 MST", cert.NotAfter)
-	if err != nil {
-		return dateRange{}, fmt.Errorf("End time parse error   - %s", err)
-	}
-
-	return dateRange{
-		Start: startTime,
-		End:   endTime,
-	}, nil
 }
